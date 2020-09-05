337bc31b57964b0bae1d69776f58842ce03bb547
makkkkkkkkks <gmaks@i.ua>
2020-09-05 19:16:07 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/service/impl/FriendsByMovieServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.FriendsByMovie;
+
+public class FriendsByMovieServiceImpl implements FriendsByMovie {
+}
---
+++ src/main/java/com/example/demo/service/impl/ReviewServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.ReviewService;
+
+public class ReviewServiceImpl implements ReviewService {
+}
