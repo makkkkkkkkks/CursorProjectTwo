265118ae3e1ca6c445c6b89a29f8be51ccde0141
Oleksandr102 <65391094+Oleksandr102@users.noreply.github.com>
2020-09-13 18:00:33 +0300

    modify classes -add code for movie entity -add category enums -add movie repo

--- src/main/java/com/example/demo/model/Movie.java
+++ src/main/java/com/example/demo/model/Movie.java
@@ -1,4 +1,33 @@
 package com.example.demo.model;
 
+import com.example.demo.enums.Category;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+    private Category category;
+    private String director;
+    private String shortDescription;
+    private int likes;
+    private float rate;
+
+    public Movie(String title, Category category, String director, String shortDescription, int likes, float rate) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.shortDescription = shortDescription;
+        this.likes = likes;
+        this.rate = rate;
+    }
 }
--- src/main/java/com/example/demo/repository/MovieRepository.java
+++ src/main/java/com/example/demo/repository/MovieRepository.java
@@ -1,4 +1,15 @@
 package com.example.demo.repository;
 
-public interface MovieRepository {
+import com.example.demo.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    Optional<Movie> getMovieById(Long id);
+
 }
