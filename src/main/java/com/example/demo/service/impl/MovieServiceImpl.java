632d128964018a6539ed81a7a6c9f410012d97b8
Artur Teben <teben.artur99@gmail.com>
2020-09-14 21:34:13 +0300

    Implement methods

--- src/main/java/com/example/demo/service/impl/MovieServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/MovieServiceImpl.java
@@ -1,6 +1,47 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.model.Movie;
+import com.example.demo.repository.MovieRepository;
 import com.example.demo.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
 public class MovieServiceImpl implements MovieService {
+
+    private final MovieRepository movieRepository;
+
+    @Override
+    public List<Movie> findAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    @Override
+    public Optional<Movie> findMovieById(Long id) {
+        return movieRepository.getMovieById(id);
+    }
+
+    @Override
+    public Movie addRateMovie(Long id) {
+        return null;
+    }
+
+    @Override
+    public void addMovie(Movie movie) {
+        movieRepository.save(movie);
+    }
+
+    @Override
+    public void deleteMovie(Long id) {
+        movieRepository.deleteById(id);
+    }
+
+    @Override
+    public void updateMovie(Long id) {
+
+    }
 }
