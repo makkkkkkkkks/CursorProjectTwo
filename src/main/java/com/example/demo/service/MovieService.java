a99bc4de4ffe62dc2e2efa5b9a5616494329a631
Artur Teben <teben.artur99@gmail.com>
2020-09-14 21:33:58 +0300

    Add methods

--- src/main/java/com/example/demo/service/MovieService.java
+++ src/main/java/com/example/demo/service/MovieService.java
@@ -1,4 +1,20 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Movie;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface MovieService {
+    public List<Movie> findAllMovies();
+
+    public Optional<Movie> findMovieById(Long id);
+
+    public Movie addRateMovie(Long id);
+
+    public void addMovie(Movie movie);
+
+    public void deleteMovie(Long id);
+
+    public void updateMovie(Long id);
 }
